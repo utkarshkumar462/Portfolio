5962b88510d92e8e4c68b00f14c9497e77020e8c
Utkarsh Gupta <146528134+utkarshkumar462@users.noreply.github.com>
2025-03-01 19:15:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -135,6 +135,13 @@ window.addEventListener("scroll", () => {
 //     let clone = skillsBox.cloneNode(true);
 //     skillsContainer.appendChild(clone);
 // });
+$(document).ready(function () {
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("fa-times"); // Change icon (bars to cross)
+    });
+});
+
 
 
 
